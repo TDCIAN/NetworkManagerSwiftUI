ed683ca115b3b079c173e76e3d72beb0e0d7d583
TDCIAN <tdcian71@gmail.com>
2021-12-30 22:06:59 +0900

    finished

--- NetworkManagerSwiftUI/ContentView.swift
+++ NetworkManagerSwiftUI/ContentView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Group {
+            if networkManager.isConnected {
+                // show main content
+            } else {
+                // show failed internet screen
+            }
+        }
+        
+        ZStack {
+            Color(.systemBlue).ignoresSafeArea()
+            
+            VStack {
+                Image(systemName: networkManager.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(Color.white)
+                
+                Text(networkManager.connectionDescription)
+                    .font(.system(size: 18))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Button(action: {
+                    networkManager.startMonitoring()
+                }, label: {
+                    Text("Check Status")
+                        .padding()
+                        .font(.headline)
+                        .foregroundColor(Color(.systemBlue))
+                })
+                .frame(width: 140)
+                .background(Color.white)
+                .clipShape(Capsule())
+                .padding()
+            }
+        }
     }
 }
--- NetworkManagerSwiftUI/NetworkManager.swift
+++ NetworkManagerSwiftUI/NetworkManager.swift
@@ -3,16 +3,27 @@
 //  NetworkManagerSwiftUI
 //
 //  Created by JeongminKim on 2021/12/30.
-//
+// 도움 출처: https://magicmon.tistory.com/229
 
 import Foundation
 import Network
 
 class NetworkManager: ObservableObject {
     let monitor = NWPathMonitor()
-    let queue = DispatchQueue(label: "NetworkManager")
-    
+
     @Published var isConnected = true
+    
+    var imageName: String {
+        return isConnected ? "wifi" : "wifi.slash"
+    }
+    
+    var connectionDescription: String {
+        if isConnected {
+            return "Internet connection looks good!"
+        } else {
+            return "It looks like you're not connected to the internet"
+        }
+    }
     
     init() {
         monitor.pathUpdateHandler = { path in
@@ -21,6 +32,23 @@ class NetworkManager: ObservableObject {
             }
         }
         
-        monitor.start(queue: queue)
+        monitor.start(queue: DispatchQueue.global())
+    }
+    
+    func startMonitoring() {
+        monitor.start(queue: DispatchQueue.main)
+        monitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            
+            if path.usesInterfaceType(.wifi) {
+                print("Using wifi")
+            } else if path.usesInterfaceType(.cellular) {
+                print("Using cellular")
+            }
+        }
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
     }
 }
